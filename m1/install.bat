e932f7e72d596b9010c21f4231992d15874601b9
Pavel Rezny <pavel.rezny@tmapy.cz>
2016-01-26 13:38:15 +0100

    Added CREATE EXTENSION postgis

--- m1/install.bat
+++ m1/install.bat
@@ -1,5 +1,6 @@
 @echo off
 call config.bat
 %PGBIN%\psql -x -c "CREATE SCHEMA utils"
+%PGBIN%\psql -x -c "CREATE EXTENSION postgis"
 %PGBIN%\psql -x -c "CREATE EXTENSION plpython3u"
 call update.bat
