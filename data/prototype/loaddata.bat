46be6b60ffb516bfd6d407d1278ee615a0517ec1
Tomas Krecmer <tokr@tmapy.cz>
2016-04-05 08:30:01 +0200

    actual data model support for load prototype data

--- data/prototype/loaddata.bat
+++ data/prototype/loaddata.bat
@@ -1,20 +1,49 @@
 @echo off
 call ../../m1/config.bat
 SET PGCONN="dbname=%PGDATABASE% user=%PGUSER% password=%PGPASSWORD%"
-%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "TRUNCATE data.features"
+
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "TRUNCATE data.element_types CASCADE"
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (1720000,2,1,'{\"buffer\":{\"radius\":0.2}}',1,true)"
+REM cesta
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (2470000,2,1,'{\"buffer\":{\"radius\":1}}',1,true)"
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (2480001,2,1,'{\"buffer\":{\"radius\":1}}',1,true)"
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (2480006,2,1,'{\"buffer\":{\"radius\":1}}',1,true)"
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (2490101,2,1,'{\"buffer\":{\"radius\":0.75}}',1,true)"
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (2490200,2,1,'{\"buffer\":{\"radius\":3}}',1,true)"
+
+REM vodni tok
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (3020100,2,1,'{\"buffer\":{\"radius\":0.75}}',1,false)"
+REM vodni kanal
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (3030000,2,1,'{\"buffer\":{\"radius\":6}}',1,true)"
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (3040000,2,1,'{\"buffer\":{\"radius\":6}}',1,false)"
+REM brehovka
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (3060000,2,1,'{\"buffer\":{\"radius\":0.75}}',1,true)"
+REM vodni plocha
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (3330000,3,1,'{\"buffer\":{\"radius\":0.75}}',1,true)"
+REM zelen
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (4120000,2,1,'{\"buffer\":{\"radius\":6}}',1,false)"
+REM hr. uzivani
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (5210100,2,1,'{\"buffer\":{\"radius\":0.75}}',1,true)"
+REM terenni stupen
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "INSERT INTO data.element_types (elt_id, geom_type, priority, footprint_params, clearance_category, topology_participant) VALUES (6060100,2,1,'{\"buffer\":{\"radius\":3.75}}',1,false)" 
+
+%OGRBIN%\ogrinfo -ro PG:%PGCONN% -sql "TRUNCATE data.elements CASCADE"
 for /R %~dp0 %%i IN (*.shp) do (
   echo %%~ni
   
-  %OGRBIN%\ogr2ogr -overwrite -f PostgreSQL PG:%PGCONN% -nlt GEOMETRY -nln data.temp_features^
+  %OGRBIN%\ogr2ogr -overwrite -f PostgreSQL PG:%PGCONN% -nlt GEOMETRY -nln data.temp_elements^
   -lco OVERWRITE=YES^
   --config PG_USE_COPY YES^
-  -sql "SELECT objectid AS feature_id, '%%~ni' as src_tbl, objectid as src_id, znacka as symbol FROM %%~ni" %%i
+  -sql "SELECT znacka as elt_id, objectid AS source_id, znacka as source_elt_id FROM %%~ni" %%i
   
   %OGRBIN%\ogrinfo -ro PG:%PGCONN%^
-  -sql "INSERT INTO data.features (feature_id, src_tbl, src_id, symbol, geom) SELECT feature_id, src_tbl, src_id, symbol, ST_SetSrid(wkb_geometry, 0) FROM data.temp_features"
+  -sql "INSERT INTO data.elements (elt_id, source_id, source_elt_id, geom) SELECT elt_id, source_id, source_elt_id, ST_SetSrid(wkb_geometry, 0) FROM data.temp_elements"
   
   %OGRBIN%\ogrinfo -ro PG:%PGCONN%^
-  -sql "DROP TABLE data.temp_features"
+  -sql "VACUUM FULL ANALYZE data.temp_elements"
+  
+  %OGRBIN%\ogrinfo -ro PG:%PGCONN%^
+  -sql "DROP TABLE data.temp_elements"
   
 )
 
