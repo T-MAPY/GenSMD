aacf55558c7589ea3c0fd455a0cbce6f994e3bf4
Tomas Krecmer <tokr@tmapy.cz>
2016-04-11 11:56:44 +0200

    dropped FK elements - element_types

--- m1/postgresql/src/data/tables_fk/elements.sql
+++
@@ -1,6 +0,0 @@
-SET search_path = data, public, pg_catalog;
-
-ALTER TABLE ONLY elements
-    ADD CONSTRAINT fk_elements_element_types FOREIGN KEY (elt_id) REFERENCES element_types(elt_id);
-
-SET search_path TO DEFAULT;
