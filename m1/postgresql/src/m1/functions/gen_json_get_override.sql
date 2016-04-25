6016d60f315bd8b97f71fecf4782ea2b4d0e2392
Tomas Krecmer <tokr@tmapy.cz>
2016-04-25 11:49:26 +0200

    removed gen_json_get_override(adata jsonb, path text, akey text)

--- m1/postgresql/src/m1/functions/gen_json_get_override.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION m1.gen_json_get_override(adata jsonb, path text, akey text)
- RETURNS jsonb
- LANGUAGE plpgsql
-AS $function$
-BEGIN
-  RETURN jsonb_set(adata, array[path], jsonb_extract_path(adata, path) || COALESCE(jsonb_extract_path(adata, 'overrides', akey, path), '{}'::jsonb));
-END;
-$function$
-;
